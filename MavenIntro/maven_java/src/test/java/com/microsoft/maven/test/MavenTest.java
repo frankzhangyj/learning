41174f668d44ca8ea6c63810314dbf9847e34fbd
frank <xxx.@x.com>
2024-10-23 18:09:32 +0800

    learn maven

---
+++ MavenIntro/maven_java/src/test/java/com/microsoft/maven/test/MavenTest.java
@@ -0,0 +1,18 @@
+package com.microsoft.maven.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description
+ * @Author frank
+ * @Date 2024/10/11
+ */
+public class MavenTest {
+    @Test
+    public void testMaven() {
+        int a = 10;
+        int b = 20;
+        //Assertions.assertEquals(a + b, 10);
+    }
+}
